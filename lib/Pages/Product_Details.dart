5279afc78c1cad3327f60bbf0010fa8d0217b700
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-12-02 13:50:00 +0200

    create product details

--- lib/Pages/Product_Details.dart
+++ lib/Pages/Product_Details.dart
@@ -1,25 +1,118 @@
 import 'package:flutter/material.dart';
 
-class Product_Details extends StatelessWidget {
+class Product_Details extends StatefulWidget {
   var product;
   Product_Details({super.key, required this.product});
 
+  @override
+  State<Product_Details> createState() => _Product_DetailsState();
+}
+
+class _Product_DetailsState extends State<Product_Details> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFEAEB),
       appBar: AppBar(),
-      bottomSheet:   BottomAppBar(
-            child: Container(
-              width: double.infinity,
-              height: 300,
-              color:  Color(0xFF835792),
-              child: Row(children: [
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: BottomAppBar(
+          color: Color(0xFF835792),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                ),
+                color: Color(0xFFFFEAEB)),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '\$ ${widget.product.price}',
+                    style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xFF835792)),
+                  ),
+                  MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          10.0), // Adjust the radius as needed
+                    ),
+                    textColor: Colors.white,
+                    color: Color(0xFF835792),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.asset(
+                                    widget.product
+                                        .image, // Replace with the path to your image
+                                    height: 200,
+                                    width: 200,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text('Dialog Title'),
+                                SizedBox(height: 10),
+                                TextField(
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    labelText: 'Enter Number',
+                                    hintText: 'Start from 1',
+                                  ),
+                                  onChanged: (value) {},
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Cancel'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  // Add your logic here to use the selected number
 
-                
-              ]),
-            ),
+                                  // Close the dialog
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Row(children: [
+                      Icon(Icons.send),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('\$ ${widget.product.price}',
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white))
+                    ]),
+                  )
+                ]),
           ),
+        ),
+      ),
       body: Container(
         width: double.infinity,
         child: Column(children: [
@@ -31,7 +124,7 @@ class Product_Details extends StatelessWidget {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
               child: Image.asset(
-                product.image,
+                widget.product.image,
                 fit: BoxFit.cover,
               ),
             ),
@@ -44,11 +137,11 @@ class Product_Details extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    product.name,
+                    widget.product.name,
                     style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    '\$ ${product.price}',
+                    '\$ ${widget.product.price}',
                     style: TextStyle(
                         fontSize: 17,
                         fontWeight: FontWeight.w700,
@@ -56,7 +149,59 @@ class Product_Details extends StatelessWidget {
                   ),
                 ]),
           ),
-        
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    child: Text(
+                      "Immerse yourself in unparalleled audio quality with HarmonyX Wireless Earbuds, the epitome of cutting-edge technology and sleek design These earbuds redefine .",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xFF835792),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "Colors Avalibel",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      CircleAvatar(radius: 20, backgroundColor: Colors.orange),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      CircleAvatar(radius: 20, backgroundColor: Colors.green),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      CircleAvatar(radius: 20, backgroundColor: Colors.teal),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      CircleAvatar(radius: 20, backgroundColor: Colors.red),
+                      SizedBox(
+                        width: 15,
+                      )
+                    ],
+                  )
+                ]),
+          )
         ]),
       ),
     );
